704fde02a9867063276494de8b8619e401ce2990
nataliasviattseva <30287691+nataliasviattseva@users.noreply.github.com>
2023-04-06 09:56:13 +0200

    urls.py changé : ajoetés les liens pour les pages pour les questions

--- DataParis/urls.py
+++ DataParis/urls.py
@@ -1,11 +1,14 @@
-
 from django.contrib import admin
 from django.urls import path
 from . import views
 
-#app_name = 'DataParis'
+# app_name = 'DataParis'
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("", views.home, name="temp"),
+    path("home/", views.home, name="home"),
+    path("question1/", views.Question_1, name="question1"),
+    path("question2", views.question2, name="question2"),
+    path("question3/", views.Question_3, name="question3"),
+
 ]
