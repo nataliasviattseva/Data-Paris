18083870e7ee1fe7745af079d0626eb5ef6e541b
nataliasviattseva <30287691+nataliasviattseva@users.noreply.github.com>
2023-04-07 00:56:49 +0200

    Re-création du structure du projet

--- DataParis/urls.py
+++ DataParis/urls.py
@@ -8,7 +8,7 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("home/", views.home, name="home"),
-    path("Question1.html", views.Question_1, name="question1"),
+    path("question1/", views.question1, name="question1"),
     path("question2/", views.question2, name="question2"),
-    path("Question3.html", views.Question_3, name="question3"),
+    path("question3/", views.question3, name="question3"),
 ]
